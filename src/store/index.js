2e5ec43ff12d74b53e8ec66c71c2be77c7538f8a
theopenfinanceproject <grosjean.alexandre@gmail.com>
2021-06-05 13:12:21 +0200

    added metamask wallet as well as wallet state

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 import exchange from './modules/exchange'
+import wallet from './modules/wallet'
 
 export default createStore({
   modules: {
     // State Management Exchange
-    exchange
+    exchange,
+    wallet
   }
 });
