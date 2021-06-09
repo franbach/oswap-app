69c022cbe4ee4fad523c78a0ab743ab727193f91
Francisco Bach <franciscobach@gmail.com>
2021-06-09 00:34:38 -0300

    Installed Toaster

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+const options = {};
+
 import PerfectScrollbar from 'vue3-perfect-scrollbar'
 import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
 
@@ -16,5 +20,6 @@ const app = createApp(App)
 app.use(router);
 app.use(store);
 app.use(PerfectScrollbar);
+app.use(Toast, options);
 
 app.mount('#oswapApp')
