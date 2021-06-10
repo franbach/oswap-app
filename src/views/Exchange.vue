6bbb0f812260c2b14aee31c495dd62e043ee4deb
Francisco Bach <franciscobach@gmail.com>
2021-06-10 18:05:32 -0300

    Removed leftovers of the warning component

--- src/views/Exchange.vue
+++ src/views/Exchange.vue
@@ -15,8 +15,6 @@
       </div>
     </transition-group>
 
-    <!-- Warning Messages -->
-    <Warning/>
   </div>
 </template>
 
@@ -24,7 +22,6 @@
 
   import Swap from '@/components/exchange/Swap'
   import Swapper from '@/components/exchange/Swapper'
-  import Warning from '@/components/exchange/Warning'
 
   import { mapGetters } from 'vuex';
 
@@ -33,7 +30,6 @@
     components: {
       Swap,
       Swapper,
-      Warning
     },
     computed: {
       ...mapGetters('exchange', ['getStepState'])
