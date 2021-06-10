83e4594231992740fa7fdd0da06b4a7ad043345c
Francisco Bach <franciscobach@gmail.com>
2021-06-10 17:36:39 -0300

    Added validation to the amount input

--- src/components/exchange/Swapper/SwapperAmount.vue
+++ src/components/exchange/Swapper/SwapperAmount.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col mt-3 rounded-xl">
     <div class="flex items-center space-x-3">
       <div class="relative flex flex-1 items-center">
-        <input type="text" v-model="amount" @input="inputValid()" class="flex flex-1 ring-1 bg-gray-100 focus:outline-none text-oswapGreen focus:ring-1 ring-black focus:ring-oswapGreen ring-opacity-5 rounded-xl py-2 items-center pl-3 dark:bg-oswapDark-gray dark:placeholder-gray-600 placeholder-gray-300" placeholder="Amount...">
+        <input type="text" v-model="amount" @input="inputChange()" class="flex flex-1 ring-1 bg-gray-100 focus:outline-none text-oswapGreen focus:ring-1 ring-black focus:ring-oswapGreen ring-opacity-5 rounded-xl py-2 items-center pl-3 dark:bg-oswapDark-gray dark:placeholder-gray-600 placeholder-gray-300" placeholder="Amount...">
         <p class="text-xs right-1 absolute bg-gray-200 dark:bg-gray-600 rounded-lg p-2 text-oswapGreen-dark">{{this.getToken()['token1'].Symbol}}</p>
       </div>
       <div @click="setMax()" class="flex items-center bg-oswapGreen dark:bg-oswapGreen-dark hover:bg-oswapGreen-dark dark:hover:bg-oswapGreen cursor-pointer p-3 rounded-xl text-gray-50 space-x-2">
@@ -42,23 +42,20 @@
       ...mapGetters('exchange', ['getToken']),
       ...mapActions('exchange', ['goTo']),
 
-      inputValid: function(){
-        let float = parseFloat(this.amount)
-        console.log(float)
-        //have to find a way to throw error if value is wrong parsefloat returns NaN or partial info
-        this.$emit("amount", this.amount);
-        let explorer = 'https://explorer.harmony.one/#/tx/'
-        let transaction = '0x5152b185a39c7d454910266d0ab7db10c892a762665151aa952e922367101bf3'
-
-        if (this.amount == 'test') {
-          toastMe('error', {
-            title: 'Transaction ID',
-            msg: transaction,
-            link: true,
-            href: `${explorer}${transaction}`
-          })
+      inputChange: function(){
+        // parseFloat seems to behave like this regex rule.
+        if (!this.amount.match(/^\d*\.?\d*$/)) {
+          toastMe('warning', {
+            title: 'Invalid input',
+            msg: 'Please insert a valid amount. e.g.: 12345.67'
+          });
+          this.amount = ''
+        } else {
+          // If theres nothing wrong with the input then emit the amount!
+          this.$emit("amount", this.amount);
         }
       },
+
       setMax: async function(){
         this.amount = await this.getTokenBalance(this.getToken()['token1'])
         this.$emit("amount", this.amount);
