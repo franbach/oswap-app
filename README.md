1404edc893b1664d10e383229fd6f5ee51477035
Francisco Bach <franciscobach@gmail.com>
2021-07-03 23:02:43 -0300

    Added preview to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OpenSwap new design proposal
+
+### Preview
+![oswap](https://github.com/franbach/oswap-app/blob/main/public/preview.webm)
--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,60 @@
 <template>
   <div id="root" :class="darkmode ? 'dark' : ''" class="antialiased pt-18">
-    <div id="header" class="w-full fixed inset-x-0 top-0 dark:bg-oswapDark-gray bg-gray-200 z-30">
+    <div
+      id="header"
+      class="w-full fixed inset-x-0 top-0 dark:bg-oswapDark-gray bg-gray-200 z-30"
+    >
       <Header />
     </div>
-  
-    <div id="body" class="flex w-full oswap-layout dark:bg-oswapDark-gray bg-gray-200">
-      <router-view/>
+
+    <div
+      id="body"
+      class="flex w-full oswap-layout dark:bg-oswapDark-gray bg-gray-200"
+    >
+      <router-view />
     </div>
 
     <div id="footer" class="w-full dark:bg-oswapDark-gray bg-gray-200">
-      <Footer @dark-mode="changeColor()" :colorMode="darkmode ? 'Dark Mode' : 'Light Mode'"/>
+      <Footer
+        @dark-mode="changeColor()"
+        :colorMode="darkmode ? 'Dark Mode' : 'Light Mode'"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
-  import Header from '@/components/Header'
-  import Footer from '@/components/Footer'
-
-  export default {
-    name: 'OpenSwap',
-    components: {
-      Header,
-      Footer
-    },
-    data() {
-      return {
-        darkmode: false,
+export default {
+  name: 'OpenSwap',
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      darkmode: false,
+    };
+  },
+  methods: {
+    changeColor() {
+      this.darkmode = !this.darkmode;
+      if (this.darkmode) {
+        document.body.classList.remove('bg-gray-200');
+        document.body.classList.add('bg-oswapDark-gray');
+      } else {
+        document.body.classList.add('bg-gray-200');
       }
     },
-    methods: {
-      changeColor() {
-        this.darkmode = !this.darkmode
-        if (this.darkmode) {
-          document.body.classList.remove('bg-gray-200')
-          document.body.classList.add('bg-oswapDark-gray')
-        } else {
-          document.body.classList.add('bg-gray-200')
-        }
-      },
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss">
-  .oswap-layout {
-    /* viewport height - header - footer */
-    min-height: calc(100vh - 72px - 64px);
-  }
+.oswap-layout {
+  /* viewport height - header - footer */
+  min-height: calc(100vh - 72px - 64px);
+}
 </style>
