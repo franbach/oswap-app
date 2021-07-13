c6d81d6154349c832315f1ec90f3167d97d17dc3
Francisco Bach <franciscobach@gmail.com>
2021-07-13 00:36:43 -0300

    Added cursor-pointer to approve

--- src/components/exchange/Swapper/SwapperApprove.vue
+++ src/components/exchange/Swapper/SwapperApprove.vue
@@ -13,7 +13,7 @@
   <transition tag="div" name="approve-btn" class="inline-block absolute">
     <div @click="approve()" v-if="this.getBtnState({approve: 'approve'})" class="flex w-28">
       <div class="grab-attention-glowing"></div>
-      <div class="grab-attention">
+      <div class="grab-attention cursor-pointer">
         <div class="flex flex-1 items-center justify-center">
           <p class="text-sm text-oswapGreen-dark dark:text-oswapGreen group-hover:text-gray-50 dark:group-hover:text-oswapDark-gray">Approve</p>
         </div>
