c5c33c551cfc474f2347256c6e5428a4a37daea3
Francisco Bach <franciscobach@gmail.com>
2021-07-14 15:27:43 -0300

    Improved rates design

--- src/components/exchange/Swapper/SwapperSelectRate.vue
+++ src/components/exchange/Swapper/SwapperSelectRate.vue
@@ -1,5 +1,8 @@
 <template>
+  <!--
   <p @click="selectRate()" :class="this.picked === this.rate ? 'ring-4 ring-gray-50 dark:ring-oswapDark-gray dark:bg-oswapGreen bg-oswapGreen-dark' : 'dark:bg-oswapGreen-dark bg-oswapGreen hover:bg-oswapGreen-dark dark:hover:bg-oswapGreen'" class="cursor-pointer text-gray-50 p-1 px-2 rounded-lg text-xs dark:text-gray-50">{{this.rate}}%</p>
+  -->
+  <p @click="selectRate()" :class="this.picked === this.rate ? 'border-l-2 border-oswapGreen' : 'hover:bg-gray-50 dark:hover:bg-gray-500'" class="cursor-pointer p-1 px-2 rounded-lg text-xs dark:text-gray-300 dark:bg-gray-600 bg-gray-100">{{this.rate}}%</p>
 </template>
 
 <script>
