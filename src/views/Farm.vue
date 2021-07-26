32f5c6839c7f7da6a062471352551d8e48576699
Francisco Bach <franciscobach@gmail.com>
2021-07-25 23:44:11 -0300

    Added loading animation to Farm view

--- src/views/Farm.vue
+++ src/views/Farm.vue
@@ -10,6 +10,12 @@
         <FarmPair  v-for="(pool, index) in Pools" :key="index" :poolData="farmData[pool.i]" :pool="pool" />
         <CustomFarmPair  v-for="(pool, index) in CustomPools" :key="index" :poolData="customData[pool.i]" :pool="pool" />
       </div>
+      <div v-else class="flex items-center mt-16">
+        <svg class="animate-spin h-8 w-8 text-oswapGreen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+      </div>
     </transition>
   </div>
 </template>
