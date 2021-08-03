f3d257e56ba9ab4f053b00d30f99842ee2611117
Francisco Bach <franciscobach@gmail.com>
2021-08-03 12:15:25 -0300

    Fixed oSWAP price and Wallet button overlap

--- src/components/header/OswapPrice.vue
+++ src/components/header/OswapPrice.vue
@@ -2,16 +2,20 @@
   <!-- Oswap token info -->
   <tooltip-me>
     <div class="flex rounded-lg focus:outline-none focus:ring-1 focus:ring-black space-x-2 p-2 px-1 md:px-3 items-center">
-      <img alt="oSwap" src="@/assets/oswap_asset.png" class="h-6">
-      <p class="text-xs dark:text-oswapGreen">${{oswapPrice}}</p>
+      <img alt="oSwap" src="@/assets/oswap_asset.png" class="ss:h-5 xs:h-6">
+      <p class="ss:hidden xs:block text-xs dark:text-oswapGreen">${{oswapPrice}}</p>
     </div>
 
-    <tooltip-me-content :options="tooltip" class="flex flex-col w-72 space-y-3 p-3 rounded-lg shadow-xl">
-      <div class="flex space-x-2 items-center">
+    <tooltip-me-content :options="tooltip" class="flex flex-col w-72 p-3 rounded-lg shadow-xl">
+      <div class="ss:flex ss:mb-3 xs:hidden ss:space-x-2 ss:items-center">
+        <img alt="oSwap" src="@/assets/oswap_asset.png" class="h-5">
+        <p class="text-sm">oSWAP Price: ${{oswapPrice}}</p>
+      </div>
+      <div class="flex space-x-2 mb-3 items-center">
         <i class="las la-coins text-xl"></i>
         <p class="text-sm">Market Cap: ${{marketCap}} USD</p>
       </div>
-      <div class="flex space-x-2 items-center">
+      <div class="flex space-x-2 mb-3 items-center">
         <i class="las la-hand-holding-usd text-xl"></i>
         <p class="text-sm">Total Supply: {{balances.totalSupply}} oSWAP</p>
       </div>
@@ -25,7 +29,7 @@
 
 <script>
   import openswap from "@/shared/openswap.js"
-import { commify } from '@ethersproject/units';
+  import { commify } from '@ethersproject/units';
 
   export default {
     name: "Oswap",
@@ -33,12 +37,15 @@ import { commify } from '@ethersproject/units';
     data() {
       return {
         tooltip: {
-          name: 'oswapInfo',
+          name: new Date().getTime(),
           position: 'bottom',
           color: 'rgba(249, 250, 251, 1)',
           offset: 12,
-          speed: 300
+          speed: 300,
+          shift: 50
         },
+        ttpObj: null,
+        ttpRec: null,
         oswapPrice: 0.00,
         balances: {
           totalSupply: 0.00,
@@ -49,12 +56,60 @@ import { commify } from '@ethersproject/units';
     },
     mixins: [openswap],
     mounted: async function() {
+      this.$nextTick(function() {
+        // Grabs the tooltip element
+        this.ttpObj = document.querySelector(`div[tooltipme="tooltip-me_${this.tooltip.name}"]`);
+        // Format the tooltip the first time
+        this.adjustTooltip();
+        // Format the tooltip when the user resizes the browser
+        window.addEventListener('resize', () => {
+          this.adjustTooltip();
+        });
+      })
+
       this.oswapPrice = await this.getOswapPrice();
       this.balances = await this.getBurnAndTotalSupply();
       this.marketCap = commify((this.balances.totalSupply * this.oswapPrice).toFixed(2));
       this.balances.totalSupply = commify(this.balances.totalSupply);
       this.balances.burnedAmount = commify(this.balances.burnedAmount);
-      
+    },
+    methods: {
+      getWindowSize() {
+        return {
+          height: window.innerHeight,
+          width: window.innerWidth
+        }
+      },
+      adjustTooltip() {
+        // gets the tooltip location bounduary
+        this.ttpRec = this.ttpObj.getBoundingClientRect();
+        // find the middle of the window
+        let width = this.getWindowSize().width;
+        let xMiddle = width / 2;
+
+        // screen size from 0 - 540
+        if (width > 0 && width < 540) {
+          // for tooltips at the middle left
+          if ((this.ttpRec.width / 2 + this.ttpRec.left) < xMiddle) {
+            this.tooltip.shift = 50
+          }
+          // for tooltips at the middle right
+          if ((this.ttpRec.width / 2 + this.ttpRec.left) > xMiddle) {
+            this.tooltip.shift = 78
+          }
+        } else if (width >= 540 && width < 1024) {
+           // for tooltips at the middle left
+          if ((this.ttpRec.width / 2 + this.ttpRec.left) < xMiddle) {
+            this.tooltip.shift = 50
+          }
+          // for tooltips at the middle right
+          if ((this.ttpRec.width / 2 + this.ttpRec.left) > xMiddle) {
+            this.tooltip.shift = 68
+          }
+        } else if (width > 1024) {
+          this.tooltip.shift = 50
+        }
+      }
     }
   }
 </script>
