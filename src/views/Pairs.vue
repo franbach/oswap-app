5f5c89fa13482c8d41ee2c14930a157f3d757cc5
Francisco Bach <franciscobach@gmail.com>
2021-08-16 00:37:15 -0300

    coming soon

--- src/views/Pairs.vue
+++ src/views/Pairs.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="pairs" class="max-w-screen-xl mx-auto flex h-full items-center justify-center xl:px-0 px-3 text-gray-500">
-    Pairs Information
+  <div id="pairs" class="relative max-w-screen-xl mx-auto items-center flex flex-1 justify-center xl:px-0 px-3 text-gray-500">
+    <transition name="horizontal" appear>
+      <div class="flex flex-col space-y-4 items-center">
+        <img src="@/assets/coming_soon2.png" class="w-64" alt="">
+        <p class="text-2xl dark:text-oswapBlue-light uppercase">coming soon !</p>
+      </div>
+    </transition>
   </div>
 </template>
 
