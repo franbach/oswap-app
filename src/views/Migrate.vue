dbc932186f77473b9685b9174639fc232d625235
Francisco Bach <franciscobach@gmail.com>
2021-08-17 12:58:04 -0300

    Started on Bridge

--- src/views/Migrate.vue
+++ src/views/Migrate.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="migrate" class="relative max-w-screen-xl mx-auto items-center flex flex-1 justify-center xl:px-0 px-3 text-gray-500">
     <transition name="horizontal" appear>
-      <div class="flex flex-col space-y-4 items-center">
-        <img src="@/assets/coming_soon2.png" class="w-64" alt="">
-        <p class="text-2xl dark:text-oswapBlue-light uppercase">coming soon !</p>
-      </div>
+      <Bridge />
     </transition>
   </div>
 </template>
 
 <script>
+  import Bridge from '@/components/bridge/Bridge';
 
-export default {
-  name: 'Migrate'
-}
+  export default {
+    name: 'Migrate',
+    components: {
+      Bridge
+    }
+  }
 </script>
