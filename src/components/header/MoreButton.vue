8e20e32e1392826c9e55cdaed3e154bf303ce583
Francisco Bach <franciscobach@gmail.com>
2021-08-22 22:07:50 -0300

    Improved menu styling to the new look

--- src/components/header/MoreButton.vue
+++ src/components/header/MoreButton.vue
@@ -15,8 +15,8 @@
     >
       <MenuItems class="absolute z-50 focus:outline-none st5-all">
         <div class="absolute z-10 w-72 mt-3 ss:-left-44 md:-left-28 lg:left-0">
-          <div class="overflow-hidden rounded-lg shadow-lg">
-            <div class="relative grid gap-8 bg-gradient-to-l from-slightGray dark:from-slightDark to-transparent p-4 md:grid-cols-1">
+          <div class="overflow-hidden rounded-lg ring-1 ring-black dark:ring-gray-600 ring-opacity-25">
+            <div class="relative grid gap-8 bg-gray-100 dark:bg-slightDark p-4 md:grid-cols-1">
               <MenuItem>
                 <router-link to="/pairs" class="flex items-center p-2 -m-3 transition group duration-150 ease-in-out rounded-lg rounded-b-lg hover:bg-gray-50 dark:hover:bg-oswapDark-gray focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
                   <div class="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white sm:h-12 sm:w-12">
